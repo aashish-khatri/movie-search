e8e61abc5b6117c92b0e93d00d34ced0f38e06e5
Aashish Khatri <aashishkhari809@gmail.com>
2020-07-18 17:32:39 +0530

    Updates

---
+++ precache-manifest.315223a90846d4e6b585eb199e28e512.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c8f0be6bc775458080537b3aaf3680",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "62fbbf938494a7014d22",
+    "url": "/movie-search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a4faa9341fc75ba3a810",
+    "url": "/movie-search/static/css/main.9e82f37d.chunk.css"
+  },
+  {
+    "revision": "62fbbf938494a7014d22",
+    "url": "/movie-search/static/js/2.f2d2b04f.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/movie-search/static/js/2.f2d2b04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4faa9341fc75ba3a810",
+    "url": "/movie-search/static/js/main.eb52c006.chunk.js"
+  },
+  {
+    "revision": "8f1314dfdc21c95c9344",
+    "url": "/movie-search/static/js/runtime-main.8345f5b5.js"
+  },
+  {
+    "revision": "24a6fdb93a3bb0275107bfc71637cb21",
+    "url": "/movie-search/static/media/logo.24a6fdb9.svg"
+  },
+  {
+    "revision": "4a50f0c7dd425f9a90f641d1e5e59afa",
+    "url": "/movie-search/static/media/poster-alt.4a50f0c7.jpg"
+  }
+]);
